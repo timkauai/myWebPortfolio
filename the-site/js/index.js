626b28b0a892df7cd6536b4628f49f55866eb932
james <jpratt20@ischool.org>
2020-05-19 21:49:49 -1000

    mid-way through styling!!

--- the-site/js/index.js
+++ the-site/js/index.js
@@ -2,14 +2,14 @@ function clicked(clicked) {
   localStorage.setItem("clicked", clicked);
 }
 
-let hiringHeadingCopy = "Here is why you should hire me:"
+let hiringHeadingCopy = "Here is why you should hire me:";
 
 class PageContent {
   constructor(PageContext) {
     let heading = document.createElement("p");
     heading.innerHTML = PageContext.pageTitle;
-    heading.className = "current-link"
-    let nav = document.getElementById('nav');
+    heading.className = "current-link";
+    let nav = document.getElementById("nav");
     nav.appendChild(heading);
     if (PageContext.hiringHeading) {
       this.createHiringHeading();
@@ -36,116 +36,141 @@ class PageContent {
 
   createHiringHeading() {
     this.hiringHeading = hiringHeadingCopy;
-    let hiringHeadingDiv = document.createElement("div");
-    const hiringHeading = (document.createElement("p").innerHTML = this.hiringHeading);
-    hiringHeadingDiv.innerHTML += hiringHeading;
-    hiringHeadingDiv.className = "main-heading"
-    document.body.appendChild(hiringHeadingDiv);
+    const hiringHeading = document.createElement("h3");
+    hiringHeading.innerHTML = this.hiringHeading;
+    hiringHeading.className = "main-heading";
+    document.body.appendChild(hiringHeading);
     return this;
   }
 
   createMeIntroOne() {
     var introOneLines = {
-      line1: "I am only 18 years old. <br/>",
-      line2: "Born and Raised on Kauai, Hawaii.<br/>",
-      line3: "I graduted from Island School.<br/>",
-      line4: "I plan on attending Santa Clara University to major in Finance.<br/>",
+      line1: "I am only 18 years old.",
+      line2: "Born and Raised on Kauai, Hawaii.",
+      line3: "I graduted from Island School.",
+      line4: "I plan on attending Santa Clara University to major in Finance.",
     };
     this.meIntroOne = introOneLines;
     let introOneDiv = document.createElement("div");
+    introOneDiv.className = "intro-one-div";
     Object.keys(introOneLines).map(function (key, index) {
       let lineText = introOneLines[key];
-      const lineElem = (document.createElement("p").innerHTML = lineText);
-      introOneDiv.innerHTML += lineElem;
-      introOneDiv.className = "introOneDiv";
+      const lineElem = document.createElement("p");
+      lineElem.innerHTML = lineText;
+      lineElem.className = "intro-one-line";
+      introOneDiv.appendChild(lineElem);
     });
     var newDiv = document.createElement("div");
-    newDiv.id = "dark";
-    document.getElementsByTagName('body')[0].appendChild(newDiv);
+    newDiv.className = "dark";
+    introOneDiv.appendChild(newDiv);
+    document.getElementsByTagName("body")[0].appendChild(introOneDiv);
     return this;
   }
   createMeIntroTwo() {
-    var obj = {
+    var introTwo = {
       line1: "I write code from a designers perspective: to look good",
       line2: "I try to learn something new with every build",
-      line3: "Turing a design into an actual product is the best part of coding",
+      line3:
+        "Turing a design into an actual product is the best part of coding",
     };
-    this.meIntroTwo = obj;
-    let introDiv = document.createElement("div");
-    Object.keys(obj).map(function (key, index) {
-      let txt = obj[key];
-      const elm = (document.createElement("p").innerHTML = txt);
-      introDiv.innerHTML += elm;
-      console.log(txt);
+    this.meIntroTwo = introTwo;
+    let introTwoDiv = document.createElement("div");
+    introTwoDiv.className = "intro-two-div";
+    Object.keys(introTwo).map(function (key, index) {
+      let lineText = introTwo[key];
+      const lineElem = document.createElement("p");
+      lineElem.innerHTML = lineText;
+      lineElem.className = "intro-two-line";
+      introTwoDiv.appendChild(lineElem);
     });
-    document.body.appendChild(introDiv);
+    var newDiv = document.createElement("div");
+    newDiv.className = "dark";
+    introTwoDiv.appendChild(newDiv);
+    document.getElementsByTagName("body")[0].appendChild(introTwoDiv);
     return this;
   }
   createSkills() {
-    var obj = {
+    var skills = {
       one: ["HTML", "95%"],
       two: ["CSS and Sass", "85%"],
       three: ["UI and UX Design", "70%"],
       four: ["JavaScript", "70%"],
       five: ["React.js", "50%"],
     };
-    this.skills = obj;
-    let introDiv = document.createElement("div");
-    Object.keys(obj).map(function (key, index) {
-      let txt = obj[key];
-      const skills = (document.createElement("p").innerHTML = txt[0]);
-      const confidence = (document.createElement("p").innerHTML = txt[1]);
-      introDiv.innerHTML += skills;
-      introDiv.innerHTML += confidence;
-      console.log(txt);
+    this.skills = skills;
+    let skillsDiv = document.createElement("div");
+    skillsDiv.className = "skills";
+    let heading = document.createElement("h3");
+    heading.innerHTML = "i've got some skills:";
+    heading.className = "heading";
+    skillsDiv.appendChild(heading);
+    let headingUnderline = document.createElement("div");
+    headingUnderline.className = "dark";
+    skillsDiv.appendChild(headingUnderline);
+    Object.keys(skills).map(function (key, index) {
+      let skillItemDiv = document.createElement("div");
+      skillItemDiv.className = "skill-item";
+      let skillText = skills[key];
+      const skillElem = document.createElement("p");
+      skillElem.innerHTML = skillText[0];
+      const confidElem = document.createElement("p");
+      confidElem.innerHTML = skillText[1];
+      skillItemDiv.appendChild(skillElem);
+      skillItemDiv.appendChild(confidElem);
+      skillsDiv.appendChild(skillItemDiv);
     });
-    document.body.appendChild(introDiv);
+    document.body.appendChild(skillsDiv);
     return this;
   }
   createJobExp() {
-    let obj = {
+    let jobExp = {
       job1: "Hokuala",
       job2: "Nalukai",
     };
-    this.jobExp = obj;
-    let introDiv = document.createElement("div");
-    Object.keys(obj).map(function (key, index) {
-      let txt = obj[key];
-      const job = (document.createElement("p").innerHTML = txt);
-      introDiv.innerHTML += job;
-      console.log(txt);
+    this.jobExp = jobExp;
+    let jobExpDiv = document.createElement("div");
+    jobExpDiv.className = "job-exp-div";
+    Object.keys(jobExp).map(function (key, index) {
+      let job = jobExp[key];
+      const jobElem = document.createElement("p");
+      jobElem.innerHTML = job;
+      jobExpDiv.appendChild(jobElem);
     });
-    document.body.appendChild(introDiv);
+    document.body.appendChild(jobExpDiv);
     return this;
   }
   createMyProjects() {
-    let obj = {
+    let projects = {
       proj1: "Randevu",
     };
-    this.myProjects = obj;
-    let introDiv = document.createElement("div");
-    Object.keys(obj).map(function (key, index) {
-      let txt = obj[key];
-      const job = (document.createElement("p").innerHTML = txt);
-      introDiv.innerHTML += job;
-      console.log(txt);
+    this.myProjects = projects;
+    let projectsDiv = document.createElement("div");
+    projectsDiv.className = "projects-div";
+    Object.keys(projects).map(function (key, index) {
+      let project = projects[key];
+      const projectElem = document.createElement("p");
+      projectElem.innerHTML = project;
+      projectsDiv.appendChild(projectElem);
     });
-    document.body.appendChild(introDiv);
+    document.body.appendChild(projectsDiv);
     return this;
   }
   createContact() {
     this.contact = "Lets get in contact";
-    let introDiv = document.createElement("div");
-    const job = (document.createElement("p").innerHTML = this.contact);
-    introDiv.innerHTML += job;
-    document.body.appendChild(introDiv);
+    let contactDiv = document.createElement("div");
+    contactDiv.className = "contact-div";
+    const contactElem = document.createElement("p");
+    contactElem.innerHTML = this.contact;
+    contactDiv.appendChild(contactElem);
+    document.body.appendChild(contactDiv);
     return this;
   }
 }
 
 class PageContext {
   constructor(
-    pageTitle, {
+    pageTitle,
+    {
       hiringHeading = false,
       meIntroOne = false,
       meIntroTwo = false,
@@ -209,11 +234,11 @@ let pageName = localStorage.getItem("clicked");
 if (pageName === "hiring") {
   const page = new PageContent(hiring);
 } else if (pageName === "learning") {
-  const page = new PageContent(learning)
+  const page = new PageContent(learning);
 } else if (pageName === "check") {
-  const page = new PageContent(check)
+  const page = new PageContent(check);
 } else if (pageName === "contact") {
-  const page = new PageContent(contact)
+  const page = new PageContent(contact);
 } else if (pageName === "work") {
-  const page = new PageContent(work)
+  const page = new PageContent(work);
 }
